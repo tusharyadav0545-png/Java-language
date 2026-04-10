f30e467089e1100fceaae8bcea2be127054ebb90
Tushar Yadav <tusharyadav0545@gmail.com>
2026-04-10 15:29:13 +0530

    new project

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
 import java.util.Scanner;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -12,7 +9,7 @@ public class Main {
             userNumber = sc.nextInt();
 
             if (userNumber == myNumber) {
-                System.out.println("WOOHOO .. CORRECT NUMBER !!! ");
+                System.out.println("CORRECT NUMBER !!! ");
                 break;
             } else if (userNumber > myNumber) {
                 System.out.println("your number is too large");
